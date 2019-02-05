3c128ec850302f23661e69e34436ce22bc3344c1
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2019-02-04 19:03:49 -0500

    Add files via upload

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -46,3 +46,55 @@ let ToggleButton = styled.input`
       transform: rotate(-45deg) translate(0, -1px);
     }
 `;
+
+let MenuLine = styled.span`
+    display: block;
+    width: 30px;
+    height: 4px;
+    margin-bottom: 5px;
+    position: relative;
+    background: white;
+    z-index: 1;
+    transform-origin: 1px 0px;
+    transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
+    background 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
+    opacity 0.55s ease;
+
+    &:first-child { transform-origin: 0% 0%; }
+    &:nth-last-child(2) { transform-origin: 0% 100%; }
+`;
+let MenuListContainer = styled.ul`
+    position: absolute;
+    width: 280px;
+    margin: -100px 0 0 0;
+    padding: 30px;
+    padding-top: 100px;
+    right: -100px;
+
+    overflow-x: hidden;
+    background: white;
+    box-shadow: 1px 1px 4px gray;
+    list-style-type: none;
+    -webkit-font-smoothing: antialiased;
+    /* to stop flickering of text in safari */
+
+    transform-origin: 0% 0%;
+    transform: translate(100%, 0);
+
+    transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0);
+
+    li {
+        padding: 10px 0;
+        font-size: 18px;
+    }
+    li:hover {
+        background-color: gray;
+        color: white;
+    }
+    li:active {
+        ${MenuLine} {
+            background: red !important;
+        }
+    }
+`;
+
