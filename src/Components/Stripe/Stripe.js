54085bdf7e14659d849efd44b2cebe9137a1daac
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2019-02-20 23:49:22 -0500

    Add files via upload

---
+++ src/Components/Stripe/Stripe.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import axios from 'axios';
+import StripeCheckout from 'react-stripe-checkout';
+
+// import STRIPE_PUBLISHABLE from './constants/stripe';
+// import PAYMENT_SERVER_URL from './constants/server';
+
+const CURRENCY = 'USD';
+
+
+const successPayment = data => { 
+  alert('Payment Successful');
+};
